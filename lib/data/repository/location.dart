db012670cc73edada65798dfbedcdcd08fb86a59
Phon Sovatanak <feelingsleepy4@gmail.com>
2025-04-20 16:46:56 +0700

    remove duplicate

--- lib/data/repository/location.dart
+++
@@ -1,6 +0,0 @@
-import 'package:race_tracker_project/model/race/location.dart';
-
-abstract class LocationRepository {
-  Future<void> addLocation(Location location);
-  Future<List<Location>> getAllLocations();
-}
