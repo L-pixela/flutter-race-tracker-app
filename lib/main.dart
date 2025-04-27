a635346ce3ce959320508837654a4c088707f784
Phon Sovatanak <feelingsleepy4@gmail.com>
2025-04-27 12:57:11 +0700

    .refacator and add the race repository, provider and update the form

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+
 import 'package:race_tracker_project/data/firebase/firebase_participant_repository.dart';
+import 'package:race_tracker_project/data/firebase/firebase_race_repository.dart';
 import 'package:race_tracker_project/data/repository/participant_repository.dart';
+import 'package:race_tracker_project/data/repository/race_repository.dart';
 import 'package:race_tracker_project/firebase_options.dart';
 import 'package:race_tracker_project/screens/manager_screen/dashboard_screen.dart';
 import 'package:race_tracker_project/screens/provider/participant_provider.dart';
-import 'package:race_tracker_project/screens/time_tracker_screen/segment_selection_screen.dart';
-import 'package:race_tracker_project/screens/time_tracker_screen/track_screen.dart';
+import 'package:race_tracker_project/screens/provider/race_provider.dart';
 import 'package:race_tracker_project/theme/theme.dart';
 
 void main() async {
@@ -19,11 +21,14 @@ void main() async {
 
   // 2. Initialize Repository for Provider
   ParticipantRepository participantRepository = FirebaseParticipantRepository();
+  RaceRepository raceRepository = FirebaseRaceRepository();
   runApp(MultiProvider(
     providers: [
       ChangeNotifierProvider(
           create: (context) =>
               ParticipantProvider(repository: participantRepository)),
+      ChangeNotifierProvider(
+          create: (context) => RaceProvider(repository: raceRepository))
     ],
     child: MyApp(),
   ));
@@ -37,39 +42,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: raceAppTheme,
-      home: Scaffold(body: SegmentSelectionScreen()),
-
-      // RegisterForm(selectedGender: '',)
-
-      // body: Column(
-
-      //   mainAxisAlignment: MainAxisAlignment.center,
-      //   children: [
-      //     /// test the race button
-      //     // Center(
-      //     //   child: RaceButton(
-      //     //     text: '09002',
-      //     //     onPressed: () => print('Button clicked!'),
-      //     //     width: 100,
-      //     //   ),
-      //     // ),
-
-      //     Image.asset("assets/images/triathlon-yellow.gif")
-      //     // AddUser("John Doe", "Acme Inc.", 30),
-      //     // SizedBox(
-      //     //   height: 15,
-      //     // ),
-      //     // GetUserByName(full_name),
-      //     // SizedBox(
-      //     //   height: 15,
-      //     // ),
-      //     // UpdateUserByName(full_name),
-      //     // SizedBox(
-      //     //   height: 15,
-      //     // ),
-      //     // DeleteUserByName(full_name),
-      //   ],
-      // )
+      home: Scaffold(body: DashboardScreen()),
     );
   }
 }
